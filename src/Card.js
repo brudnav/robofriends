d663b90ad9ea3a0363a446d95df54ec9c789f311
brudnav <brudna.vaclav@seznam.cz>
2021-01-11 00:08:54 +0100

    robots.js + Card upgrade

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 
-const Card = () => 
+const Card = (props) => 
 {
+    const {name,email,id} = props;
     return(
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src="https://robohash.org/sdf?200x200" alt=""/>
+            <img src={`https://robohash.org/${id}?200x200`} alt=""/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@email.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Jane Doe",
+        username: "J",
+        email: "jane.doe@email.com"
+    },
+    {
+        id: 2,
+        name: "John Doe",
+        username: "JD",
+        email: "john.doe@email.com"
+    },
+    {
+        id: 3,
+        name: "John Boe",
+        username: "JB",
+        email: "john.boe@email.com"
+    }
+]
