4319e96c0f2227436fdf5cdd556c6ceccf51ece0
brudnav <brudna.vaclav@seznam.cz>
2021-01-11 05:15:35 +0100

    fetchovani jmen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import {robots} from "./robots";
+//import {robots} from "./robots";
 import SearchBox from "./SearchBox"
 
 
@@ -12,10 +12,24 @@ class App extends Component
         super()
         this.state = 
         {
-            robots: robots,
+            robots: [],
             searchfield: ""
         }
     }
+
+    componentDidMount()
+    {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response=>
+            {
+               return response.json();
+            })
+            .then(users => 
+                {
+                    this.setState({robots: users});
+                })
+    }
+
 
     onSearchChange = (event)=>
     {
@@ -29,13 +43,23 @@ class App extends Component
         {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return(
-            <div>
-            <h1>RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <CardList robots={filteredRobots}/>
-            </div>
-        );
+
+        if(this.state.robots.length === 0)
+        {
+            return <h1>Loading</h1>
+        }
+        else
+        {
+            return(
+                <div>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+                </div>
+            ); 
+        }
+
+       
     }
 
 
