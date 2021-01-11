d6d0b4e96d34d85c0905b3be1c5d6b01507c40f8
brudnav <brudna.vaclav@seznam.cz>
2021-01-11 16:02:17 +0100

    new Scroll Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 //import {robots} from "./robots";
-import SearchBox from "./SearchBox"
+import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
 
 
 
@@ -54,7 +55,10 @@ class App extends Component
                 <div>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <Scroll>
+                   <CardList robots={filteredRobots}/> 
+                </Scroll>
+                
                 </div>
             ); 
         }
